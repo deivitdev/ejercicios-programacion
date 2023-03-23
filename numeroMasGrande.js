24685d465e59229a5f175ce90929f8d73e9331f2
David Salazar <david.salazar@mercadolibre.com>
2023-03-23 19:33:30 -0300

    Se agrega el ejercicio numeroMasGrande

---
+++ numeroMasGrande.js
@@ -0,0 +1,8 @@
+/* Escribe un programa para encontrar el número mayor de una lista de números.
+*/
+function encuentraElNumeroMayor( listaNumeros) {
+  // Escribe tu código aquí
+}
+// estos son los tests para probar que lo que hiciste este bien
+console.log(encuentraElNumeroMayor([8, 7, 10, 5, 6, 4, 3, 2, 1, 0])) // 10
+console.log(encuentraElNumeroMayor([2,5,3,4,1,6,7,8])) // 8

70f91881fddf35919dd4d4dce46d129970a1a7b7
David Salazar <david.salazar@mercadolibre.com>
2023-03-23 21:58:11 -0300

    se agrega otro ejecricio

---
+++ Ejercicios Javascript/2-numeroPorPalabra.js
@@ -0,0 +1,14 @@
+/* 
+Crear un programa que devuelva los array de números del 1 al 15, 
+pero que, para los múltiplos de 4, imprima la palabra "pin" en lugar del número.
+
+Para este ejercicio, debes usar for, if, array, array.push()
+*/
+
+
+function numeroPorPalabra() {
+    // Escribe tu código aquí
+}
+
+console.log(numeroPorPalabra()) 
+// [ 1, 2, 3, 'pin', 5, 6, 7, 'pin', 9, 10, 11, 'pin', 13, 14, 15 ]

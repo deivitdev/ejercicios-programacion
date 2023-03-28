e9edad622f245984b1715c952dfaf255ac53ef11
David Salazar <david.salazar@mercadolibre.com>
2023-03-28 17:56:33 -0300

    Solucion Ej 2

--- Ejercicios Javascript/2-numeroPorPalabra.js
+++ Ejercicios Javascript/2-numeroPorPalabra.js
@@ -7,8 +7,16 @@ Pista 2: investiga el método push() para agregar un elemento a un array.
 
 */
 function numeroPorPalabra() {
-    // Escribe tu código aquí
+  // Escribe tu código aquí
+  let arr = [];
+  for (let i = 1; i <= 15; i++)
+    if (i % 4 == 0) {
+      arr.push('pin');
+    } else {
+      arr.push(i);
+    }
+  return arr;
 }
 
-console.log(numeroPorPalabra()) 
+console.log(numeroPorPalabra());
 // [ 1, 2, 3, 'pin', 5, 6, 7, 'pin', 9, 10, 11, 'pin', 13, 14, 15 ]
